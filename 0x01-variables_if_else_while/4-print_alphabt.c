3b9f33db968728f19514994694b7cd7b1048cf1c
John-Ray221 <johnrayhendricks103@gmail.com>
2023-03-17 16:01:09 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,19 +7,16 @@
  */
 int main(void)
 {
-	char ch;
+	char ch = 'a';
 
-	for (ch = 'a'; ch <= 'z'; ch++)
-		if (ch == 'q' || ch == 'e')
-		{
-			ch++;
-		}
-		else
+	while (ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q')
 		{
 			putchar(ch);
 		}
+		ch++;
 	}
 	putchar('\n');
-
 	return (0);
 }
