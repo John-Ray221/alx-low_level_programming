1bfc362645a34e5785e3525049cfb2f95d5c4124
John-Ray221 <johnrayhendricks103@gmail.com>
2023-03-17 14:29:14 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,9 +4,11 @@
 /* more headers goes there */
 
 /**
- * Main: Is the starting point
- * Return: return zero meaning it is successful
- * */
+ * main - assigns a random number to int n everytime
+ * it executes, and prints it
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	int n;
@@ -16,15 +18,15 @@ int main(void)
 	/* your code goes there */
 	if (n < 0)
 	{
-		printf ("%d is negative\n", n);
+		printf("%d is negative\n", n);
 	}
 	else if (n > 0)
 	{
-		printf ("%d is positive\n", n);
+		printf("%d is positive\n", n);
 	}
 	else
 	{
-		printf ("%d is zero\n", n);
+		printf("%d is zero\n", n);
 	}
 	return (0);
 }
