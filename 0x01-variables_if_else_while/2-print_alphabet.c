9500ff15fe18354a150c0e6c1f887fe479b2a917
John-Ray221 <johnrayhendricks103@gmail.com>
2023-03-17 14:50:26 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints the last digit of the random
+ * number stored in the variable n
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	for (char i = a; i <= z; i++)
+	{
+		putchar(i);
+	}
+	putchar("\n");
+	return (0);
+}
