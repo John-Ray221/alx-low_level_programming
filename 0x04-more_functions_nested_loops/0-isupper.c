cdb16bc1f8332797f127edfc72fb0227109b30dc
John-Ray221 <johnrayhendricks103@gmail.com>
2023-03-23 15:18:11 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char c;
+
+	c = 'A';
+	printf("%c: %d\n", c, _isupper(c));
+	c = 'a';
+	printf("%c: %d\n", c, _isupper(c));
+	return (0);
+}
+
+int _isupper(int c)
+{
+	if (c > 95 && c < 122)
+	{
+		return (0);
+	}
+	else if (c > 65 && c < 90)
+	{
+		return (1);
+	}
+}
